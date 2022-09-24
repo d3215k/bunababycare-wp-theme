d0c60c841af7bbe3ca5e8aa2664a688c42130dbf
Dede Iskandar <d3215k@gmail.com>
2022-09-24 09:14:56 +0700

    wip

--- header.php
+++ header.php
@@ -74,8 +74,7 @@
 			<!-- Start introduction -->
 			<div class="container pb-12 mx-auto my-12 border-b">
 				<h1 class="text-lg font-bold text-pink-600">Bunababycare</h1>
-				<h2 class="my-4 text-3xl font-extrabold tracking-tight lg:text-7xl">Rapidly build your WordPress theme
-					with <a href="https://tailwindcss.com" class="text-primary">Tailwind CSS</a>.</h2>
+				<h2 class="my-4 text-3xl font-extrabold tracking-tight lg:text-7xl">Let's build something great!.</h2>
 				<p class="max-w-screen-lg mb-10 text-lg font-medium text-gray-700">TailPress is your go-to starting
 					point for developing WordPress themes with TailwindCSS and comes with basic block-editor support out
 					of the box.</p>
